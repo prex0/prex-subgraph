46f51b47035ba5520676c1a4a81413eac698ab93
syuhei176 <syuhei176@gmail.com>
2025-01-02 18:19:30 +0900

    update FanController

--- src/FanController.ts
+++ src/FanController.ts
@@ -5,9 +5,14 @@ import {
 } from '../generated/FanController/FanController'
 import { PumToken, PumTokenOrders } from '../generated/schema'
 import { ensureEndUser } from './helpers'
+import { CommunityPool } from '../generated/FanController/CommunityPool'
+import { FanController } from '../generated/FanController/FanController'
 
 export function handleTokenIssued(event: TokenIssued): void {
-  const pumToken = ensurePumToken(event.params.communityToken, event.block.timestamp)
+  const pumToken = ensurePumToken(
+    event.params.communityToken,
+    event.block.timestamp
+  )
   const endUser = ensureEndUser(event.params.issuer, event.block.timestamp)
 
   pumToken.name = event.params.name
@@ -18,12 +23,17 @@ export function handleTokenIssued(event: TokenIssued): void {
 }
 
 export function handleOrderFilled(event: OrderFilled): void {
-  const pumToken = ensurePumToken(event.params.communityToken, event.block.timestamp)
+  const pumToken = ensurePumToken(
+    event.params.communityToken,
+    event.block.timestamp
+  )
   const endUser = ensureEndUser(event.params.sender, event.block.timestamp)
+  const recipient = ensureEndUser(event.params.recipient, event.block.timestamp)
 
   const pumTokenOrder = new PumTokenOrders(event.transaction.hash.toHex())
   pumTokenOrder.token = pumToken.id
   pumTokenOrder.swapper = endUser.id
+  pumTokenOrder.recipient = recipient.id
   pumTokenOrder.isBuy = event.params.isBuy
   pumTokenOrder.amountIn = event.params.amountIn
   pumTokenOrder.amountOut = event.params.amountOut
@@ -31,8 +41,17 @@ export function handleOrderFilled(event: OrderFilled): void {
   pumTokenOrder.createdAt = event.block.timestamp
 
   pumTokenOrder.save()
-}
 
+
+  // Update market cap
+  const fanController = FanController.bind(event.address)
+  const communityPool = CommunityPool.bind(fanController.communityPool())
+  const marketInfo = communityPool.getMarketInfo(event.params.communityToken)
+
+  pumToken.marketCap = marketInfo.reserveStable
+
+  pumToken.save()
+}
 
 function ensurePumToken(address: Bytes, timestamp: BigInt): PumToken {
   const id = address.toHex()
@@ -41,6 +60,7 @@ function ensurePumToken(address: Bytes, timestamp: BigInt): PumToken {
   if (!pumToken) {
     pumToken = new PumToken(id)
     pumToken.address = address
+    pumToken.marketCap = BigInt.fromI32(0)
     pumToken.createdAt = timestamp
   }
 
